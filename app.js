0aba31e93c9a59e0c8f42a66719c23e4e0fa44d8
Mahesan <mahesanp789@gmail.com>
2021-07-01 14:24:44 +0530

    Mailgun API Added

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const fs = require("fs");
 const lo = require("lodash");
-const nodemailer = require("nodemailer");
 
 const app = express();
 
@@ -82,39 +81,25 @@ app.post("/contact", function(req, res){
     <p>${req.body.message}</p>
     `;
 
-    let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-        user: 'mahesanp789@gmail.com', // generated ethereal user
-        pass: 'mahesan   king'  // generated ethereal password
-    },
-    tls:{
-      rejectUnauthorized:false
-    }
-  });
+    var api_key = '5e4e3bdb49fa67b56d72f6b9bf68dc68-aff8aa95-9b1abc7b';
+    var domain = 'sandboxf2473e62392e47bdae2c6fd2fd5e8840.mailgun.org';
+    var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
-// setup email data with unicode symbols
-    let mailOptions = {
-      from: '"Nodemailer Contact" <mahesanp789@gmail.com>', // sender address
-      to: 'mahesan.cs20@bitsathy.ac.in', // list of receivers
-      subject: 'Node Contact Request', // Subject line
-      text: 'Hello world?', // plain text body
-      html: output // html body
-  };
+    var data = {
+      from: 'Mahesan <mahesan.cs20@bitsathy.ac.in>',
+      to: 'mahesan.cs20@bitsathy.ac.in',
+      subject: 'New Contact Details',
+      html: output,
+    };
 
-  // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
+    mailgun.messages().send(data, function (error, body) {
+        if (error){
+            console.log(error);
         }
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-        res.render('contact', {msg:'Email has been sent'});
+      console.log(body);
+      res.render("contact", {msg: "Email has been sent"});
     });
-})
+});
 
 function readData(){
     let JsonData = fs.readFileSync("data.json");
